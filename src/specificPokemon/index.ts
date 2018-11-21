3c396b1902604611a27b5c528276b173fc118342
Jonathan Chang <prancingwithponies@live.com>
2018-11-21 13:13:23 -0800

    parse evolutions

--- src/specificPokemon/index.ts
+++ src/specificPokemon/index.ts
@@ -19,6 +19,29 @@ const getPokemonData = (pokemon) => {
     rp(options)
       .then(($) => {
         const pokemonProperties = {
+          evolutions: (() => {
+            const evolutions = [];
+            $('.infocard-list-evo .infocard').each((index, element) => {
+              const name = $(element).find('.ent-name').text();
+
+              if(name === '') {
+                const level = $(element).find('small').text();
+                evolutions[index - 1].level = level.match(/\d+/)[0];
+              }
+
+              const types = [];
+              $(element).find('.itype').each((index, element) => {
+                types.push($(element).text());
+              });
+              evolutions.push({
+                name,
+                types
+              });
+            });
+            return evolutions.filter((item) => {
+              return item.name;
+            });
+          })(),
           locations: parseWhereToFind($)
         };
         resolve({...pokemon, ...pokemonProperties});
@@ -33,6 +56,6 @@ const getPokemonData = (pokemon) => {
 export const getAllPokemonData = () => {
   const pokemon = pokemonList[0];
   getPokemonData(pokemon).then((res) => {
-    console.log(res);
+    console.log(JSON.stringify(res, null, 2));
   });
 };
